8b8bde5e7b06acb2bc0e4643e6af3faf7ca46470
Furia-78 <matosgabriel507@gmail.com>
2026-05-06 19:19:55 -0300

    furia

---
+++ ATIVIDADE 5/exce1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+   int i, codigo;
+   in validos = 0, invalidos = 0;
+   
+   // implemente aqui um for para ler exatamente 10 codigos.
+   // Para cada codigo verificaque:
+   // se codigo >= 1000 e codigo <= 9999 incremente validados.
+   // Caso contrario, incremente invalidos.
+
+   printf ("codigos validos:  %d", validos);
+   printf ("Codigos invalidos:%d", invalidos);
+
+    return 0;
+}
