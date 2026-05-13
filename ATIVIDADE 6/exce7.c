d228f4c3ee11c0cea39e33cdbc83c2d7810b0d05
Furia-78 <matosgabriel507@gmail.com>
2026-05-13 19:32:42 -0300

    Atualizar o exce7.c

--- ATIVIDADE 6/exce7.c
+++ ATIVIDADE 6/exce7.c
@@ -1,9 +1,20 @@
+#include <stdio.h>
+
 int main() {
-    int numeros [18];
+    int numeros[18];
     int i;
-    
+
     // Leia os valores
+    for(i = 0; i < 18; i++) {
+        printf("Digite o %dº numero: ", i + 1);
+        scanf("%d", &numeros[i]);
+    }
+
     // Exiba em ordem inversa
-   
+    printf("\nNumeros em ordem inversa:\n");
+    for(i = 17; i >= 0; i--) {
+        printf("%d ", numeros[i]);
+    }
+
     return 0;
 }
