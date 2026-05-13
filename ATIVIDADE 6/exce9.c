d6e31ba8499e6356700f541297b530d8767098b0
Furia-78 <matosgabriel507@gmail.com>
2026-05-13 19:33:59 -0300

    Atualizar o exce9.c

--- ATIVIDADE 6/exce9.c
+++ ATIVIDADE 6/exce9.c
@@ -3,11 +3,31 @@
 int main() {
     int a[5], b[5], c[5];
     int i;
-    
+
     // Leia o vetor a
+    printf("Digite os valores do vetor A:\n");
+    for(i = 0; i < 5; i++) {
+        printf("A[%d]: ", i);
+        scanf("%d", &a[i]);
+    }
+
     // Leia o vetor b
+    printf("\nDigite os valores do vetor B:\n");
+    for(i = 0; i < 5; i++) {
+        printf("B[%d]: ", i);
+        scanf("%d", &b[i]);
+    }
+
     // Some os vetores
+    for(i = 0; i < 5; i++) {
+        c[i] = a[i] + b[i];
+    }
+
     // Exiba o vetor c
-   
+    printf("\nVetor C (soma):\n");
+    for(i = 0; i < 5; i++) {
+        printf("%d ", c[i]);
+    }
+
     return 0;
 }
