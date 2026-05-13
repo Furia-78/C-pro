3440a14c4573b772bc594c98121f456e48a13269
Furia-78 <matosgabriel507@gmail.com>
2026-05-13 19:34:46 -0300

    Atualizar o exec2.c

--- ATIVIDADE 6/exec2.c
+++ ATIVIDADE 6/exec2.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int numeros [8;]
+    int numeros[8]; // corrigido
     int i, soma = 0;
+
     // Leia os valores
-    // Calcule a soma
-    printf ("soma = %d\n" soma);
+    for(i = 0; i < 8; i++) {
+        printf("Digite o %dº numero: ", i + 1);
+        scanf("%d", &numeros[i]);
+        soma += numeros[i]; // já soma aqui
+    }
+
+    // Exibe a soma
+    printf("Soma = %d\n", soma); // corrigido
+
     return 0;
 }
