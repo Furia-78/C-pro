f02f7bed93cab223c30164a38cd704051bd5c740
Furia-78 <matosgabriel507@gmail.com>
2026-05-13 19:31:07 -0300

    Atualizar o exce5.c

--- ATIVIDADE 6/exce5.c
+++ ATIVIDADE 6/exce5.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    float numeros [6;]
+    float numeros[6]; // corrigido
     int i;
     float soma = 0, media;
-    
+
     // Leia os valores
+    for(i = 0; i < 6; i++) {
+        printf("Digite o %dº numero: ", i + 1);
+        scanf("%f", &numeros[i]);
+        soma += numeros[i]; // já soma aqui
+    }
+
     // Calcule a media
-    
-    printf ("Media = %.2f\n" media);
+    media = soma / 6;
+
+    printf("Media = %.2f\n", media); // corrigido
+
     return 0;
 }
