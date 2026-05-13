ff4044e4dc947b5e360fd9b0cb76880e58c028b2
Furia-78 <matosgabriel507@gmail.com>
2026-05-13 19:36:20 -0300

    Atualizar o exec4.c

--- ATIVIDADE 6/exec4.c
+++ ATIVIDADE 6/exec4.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int numeros [12;]
+    int numeros[12]; // corrigido
     int i, pares = 0;
+
     // Leia os valores
+    for(i = 0; i < 12; i++) {
+        printf("Digite o %dº numero: ", i + 1);
+        scanf("%d", &numeros[i]);
+    }
+
     // Conte os pares
-    printf ("Quantidade de pares = %d\n" pares);
+    for(i = 0; i < 12; i++) {
+        if(numeros[i] % 2 == 0) {
+            pares++;
+        }
+    }
+
+    printf("Quantidade de pares = %d\n", pares);
+
     return 0;
 }
